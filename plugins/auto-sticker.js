ebd73219880e392322ad4f4c16fe92c6cee08368
amosi12 <consolataantimi@gmail.com>
2025-08-19 22:05:48 +0300

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const config = require('../config');
+const { cmd } = require('../command');
 
-//auto voice
+// Auto Sticker
 cmd({
   on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
+}, async (conn, mek, m, { from, body, isOwner }) => {
+    if (config.AUTO_STICKER !== 'true') return; // check if auto sticker is enabled
+
     const filePath = path.join(__dirname, '../data/autosticker.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    const lowerBody = body.toLowerCase();
+
     for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            
-            if (config.AUTO_STICKER === 'true') {
-                //if (isOwner) return;        
-                await m.reply(data[text])
-            
-            }
+        if (lowerBody === text.toLowerCase()) {
+            // Reply with sticker URL from JSON
+            await m.reply({ sticker: { url: data[text] } });
+            break; // Stop after first match
         }
-    }                
+    }
 });
