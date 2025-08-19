a6c3a4cc82c95ca002906820cbc6c88ce286eaac
amosi12 <consolataantimi@gmail.com>
2025-08-19 14:26:45 +0300

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -6,7 +6,7 @@ cmd({
   on: 'body'
 }, async (conn, mek, m, { from, body }) => {
   try {
-    const jsonUrl = 'https://raw.githubusercontent.com/XdTechPro/KHAN-DATA/main/autosticker.json';
+    const jsonUrl = 'https://github.com/novaxmd/BMB-XMD-DATA/raw/refs/heads/main/autosticker.json';
     const res = await axios.get(jsonUrl);
     const data = res.data;
 
@@ -17,7 +17,7 @@ cmd({
             from,
             {
               sticker: { url: data[keyword] },
-              package: 'CASEYRHODES-XMD'
+              package: 'NEXUS-XMD'
             },
             { quoted: mek }
           );
